2c360b111f86604a151b7b1ac17faa53764454f0
irhamjaya <hino_weleh@hotmail.com>
2018-12-10 13:44:02 +0700

    aaa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
         <body>
       <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-        <a class="navbar-brand haha" href="#">FootBall</a>
+        <a class="navbar-brand" href="#">FootBall</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
